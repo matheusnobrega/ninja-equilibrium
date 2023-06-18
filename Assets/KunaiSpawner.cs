1a0bf89f695b979be243c2ac6d808ae5ff84e7f6
Zindare <henriquesantanna7@gmail.com>
2023-06-17 22:09:04 -0300

    Spawner Manager
    fiz o manager dos spawners, falta vincular com o do vento. Só faz o da kunai periodicamente

---
+++ Assets/KunaiSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+
+    public GameObject Kunai;
+    
+
+    public void spawnKunai()
+    {
+
+        Instantiate(Kunai, new Vector3(transform.position.x, transform.position.y, 0)
+            , transform.rotation);
+
+        // new Vector3 = necess�rio quando vou colocar meus n�meros no vetor
+        // transform.position.x = spawna em cima do spawner
+    }
+}
---
+++ Assets/SpawnerManagerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PipeMiddleScript : MonoBehaviour
+{
+
+    public PipeSpawnScript kunaiSpawner; // scipt pra spawnar kunais
+    public WindSpawnerScript windSpawner;
+    public float timer = 0;
+    public int spawnRate = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kunaiSpawner = GameObject.FindGameObjectWithTag("KunaiSpawn").GetComponent<PipeSpawnScript>();
+        windSpawner = GameObject.FindGameObjectWithTag("WindSpawn").GetComponent<WindSpawnerScript>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            kunaiSpawner.spawnKunai();
+            timer = 0;
+        }
+
+    }
+
+   
+}
