00f59a30bcc2afacfc8e4fcd3beb6a06e003c907
matheusnobrega <matheus.nobrega.duarte@gmail.com>
2023-06-18 12:42:45 -0300

    Imagem do vento flipa para indicar direção

--- Assets/WindSpawnerScript.cs
+++ Assets/WindSpawnerScript.cs
@@ -7,6 +7,7 @@ public class WindSpawnerScript : MonoBehaviour
     public GameObject windPrefab;
     private GameObject wind;
     private AreaEffector2D areaEffector;
+    private SpriteRenderer spriteRenderer;
     public float minSpawnTime = 5f;
     public float maxSpawnTime = 15f;
     public float minMagnitude = 1f;
@@ -31,10 +32,13 @@ public class WindSpawnerScript : MonoBehaviour
 
             areaEffector.forceMagnitude = Random.Range(minSpawnTime, maxSpawnTime);
 
+            spriteRenderer = wind.GetComponent<SpriteRenderer>();
+
             int direction = Random.Range(0, 2);
             if (direction == 0)
             {
                 areaEffector.forceAngle = 0;
+                spriteRenderer.flipX = !spriteRenderer.flipX;
             }
             else
             {
